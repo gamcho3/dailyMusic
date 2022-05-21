4a0bb231a8abed0a46fec55e10b2b621f058ce04
yongjin park <70016523+gamcho3@users.noreply.github.com>
2022-05-21 17:33:11 +0900

    a

--- lib/services/youtubeData.dart
+++ lib/services/youtubeData.dart
@@ -1,24 +1,8 @@
-import 'package:flutter/services.dart';
-import 'package:googleapis/youtube/v3.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
+import 'package:http/http.dart' as http;
 
 class YoutubeServices {
   static Future getYoutubeData() async {
-    try {
-      final googleSignIn = GoogleSignIn(
-        scopes: <String>[YouTubeApi.youtubeReadonlyScope],
-      );
-      print(googleSignIn.currentUser);
-      var httpClient = (await googleSignIn.authenticatedClient())!;
-
-      var youTubeApi = YouTubeApi(httpClient);
-      var favorites = await youTubeApi.playlistItems
-          .list(['snippet'], playlistId: 'LL', $fields: "bts" // Liked List
-              );
-      //print(favorites);
-    } on PlatformException catch (e) {
-      print(e);
-    }
+    var response = await http.get(Uri.parse(
+        'https://www.googleapis.com/youtube/v3/search?key=AIzaSyBOABnW6QiU6KRfD3BtmnF-iSJAEyyzS0Q'));
   }
 }
